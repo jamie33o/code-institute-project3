e10163fe5b7de0f18449f40bb03d9069f7d630fd
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-11 14:39:20 +0100

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(10,25, "hello world")
+    stdscr.refresh()
+    stdscr.getch()
+
+wrapper(main)
+
+
+
