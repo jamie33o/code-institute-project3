8f4e8803012d486c019b15b3c2013e424e80f345
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-12 17:18:50 +0100

    Feature: added number-generator module for creating list of random numbers
    Also added function for calling the hundred numbers 1 after the other

---
+++ number_generator.py
@@ -0,0 +1,9 @@
+import random
+
+def list_random_numbers(range_start,range_end,amount):
+    # Create a range of numbers from 0 to 100
+    number_range = range(range_start, range_end)  
+
+    # Generate a list of 100 unique random numbers
+    unique_random_numbers = random.sample(number_range, amount)
+    return unique_random_numbers
--- run.py
+++ run.py
@@ -1,15 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import time
+from number_generator import list_random_numbers
+delay_seconds = 2  # Set the delay time in seconds
+repeat_count = 5   # Sets the number of times to repeat the function call
+
+number_list = list_random_numbers(1,91,90)
 
 def main():
-  print("Would you like to add or subtract?")
-  answer = input("if you want to subtract type S if you want add type A: S/A? : ")
-  num1 =  int(input("Number 1: "))
-  num2 = int(input("Number 2: "))
-
-  result =  num1 + num2  if answer == "A" else num1 - num2
-  print(f"Your answer is: {result}")
+    print("Welcome to MEGA BINGO the game is about to start get ready!!!")
+    print("The first number is: ")
+    for i in number_list :
+        number_calls(number_list[i])  # Call the function
+        time.sleep(delay_seconds)  # Delay for the specified time
 
 
+
+def number_calls(number_called):
+    print(number_called)
+    print("Next we have: ")
+
+
+    
 main()
