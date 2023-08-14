d3c7757f005b057c9476d036a374235a29dc90b1
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-14 14:32:15 +0100

    Feature: added function to create pdf from the number lists generated
    This will be sent as attachment in an email to the user

--- create_book.py
+++ create_book.py
@@ -1,8 +1,13 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from number_generator import list_random_numbers
+from email_sender import send_email
 import random
+from io import BytesIO
+from reportlab.lib.pagesizes import letter
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
+# sets what im authorized to use with google cloud services
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -15,6 +20,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # open the google sheets for mega bingo
 SHEET = GSPREAD_CLIENT.open("mega-bingo")
+
 # get the bingo book sheet
 bingo_book_sheet = SHEET.worksheet("bingo-book")
 # gets the numbers called sheet
@@ -25,7 +31,7 @@ def create_bingo_book():
     # creates a list of 90 numbers
     book_numbers = list(range(1,91))
     # list of lists of numbers for each row of the bingo book sheet and spaces
-    elements_at_index = []
+    book_rows_list = []
 
     # Sort the numbers into groups of 10 e.g. 1-10 ,11-20
     grouped_numbers = [book_numbers[i:i+10] for i in range(0, len(book_numbers), 10)]
@@ -39,19 +45,50 @@ def create_bingo_book():
     # Iterate through each index of the list of numbers and creates a new list that match the rows so 
     # first number will be between 1-10 second will be between 11-20 and so on so then first column numbers will be 1-10 second 11-20
     for index in range(18):
-       elements_at_index.append([group[index] for group in grouped_numbers])
+       book_rows_list.append([group[index] for group in grouped_numbers])
 
-    # this adds the list of empty strings to match the empty space after every 3 rows in the bingo book
-    for index in [3, 7, 11,15,19]:      
-        elements_at_index.insert(index,[""] * 7 )
+    # adds the name and book id to first row
+    book_rows_list.insert(0,["Mega Bingo\nBook ID: 123"])
+    
+    # this adds the list of empty strings to match the merged rows after every 3 rows in the bingo book
+    for index in [4, 8, 12,16,20]:      
+        book_rows_list.insert(index,[""] * 7 )
         
     # this appends the rows to the bingo book in google sheets
-    bingo_book_sheet.append_rows(elements_at_index)
+    #bingo_book_sheet.append_rows(book_rows_list)
+    pdf_buffer = create_pdf(book_rows_list)
+    send_email("GIFTSFORYOU83@GMAIL.COM","bingobook.pdf", pdf_buffer)
 
        
+def create_pdf(row_list):   
+    """creates a bingo book pdf with the numberers generated and calls email_sender 
+    to send the pdf as attachment in an email to the users email"""
+
+    # store pdf created in bytes
+    pdf_buffer = BytesIO()
+    # Create a PDF document
+    doc = SimpleDocTemplate(pdf_buffer, pagesize=letter)
+    elements = []
     
-
-
-
-
-        
+    # Create a grid table from row lists data
+    table = Table(row_list)
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.white),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.red),
+        ("SPAN", (0,0), (8,0)),
+        ("SPAN", (0,4), (8,4)),
+        ("SPAN", (0,8), (8,8)),
+        ("SPAN", (0,12), (8,12)),
+        ("SPAN", (0,16), (8,16)),
+        ("SPAN", (0,20), (8,20)),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+    elements.append(table)
+    
+    # Build PDF
+    doc.build(elements)
+    return pdf_buffer
