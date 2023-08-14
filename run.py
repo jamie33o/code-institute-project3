2bd6c4312f322020077715a44ecfedabcd9ca650
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-14 14:35:39 +0100

    Update: move code in main function to new start game function

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import time
 from number_generator import list_random_numbers
-from email_sender import send_email
 from create_book import create_bingo_book
 
 
@@ -16,12 +15,16 @@ called_numbers_list = []
 def main():
     """ main fuction calls the number calls function in a for loop until all the numbers are called"""
     create_bingo_book()
+   
+
+def start_game():
     print("Welcome to MEGA BINGO the game is about to start get ready!!!")
     print("The first number is: ")
     for number in number_list :
-        number_calls(number)  # Call the function
+        number_calls(number)  # Call the number calls function
         time.sleep(delay_seconds)  # Delay for the specified time
-        called_numbers_list.append(number)
+        called_numbers_list.append(number) # appends the numbers called to the list 
+
 
 def number_calls(number_called):
     """prints the next number to the terminal"""
@@ -30,8 +33,6 @@ def number_calls(number_called):
         print("Game over!!")    
     else:
         print("Next we have: ")
-
-
 
 
 main()
