ee5ac3cb8c0d12645150e4531e28b68ec732f44e
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-22 18:15:07 +0100

    Update: added more comments

--- game_state.py
+++ game_state.py
@@ -27,38 +27,45 @@ class BingoGame:
         elif self.state == "calling_numbers":
             print("First we are looking for a single line")
             self.set_state("match_single_line")
+            # start calling th numbers
             number_generator.call_numbers()
-
         elif self.state == "match_single_line":
+            # check for single line
             if check_for_sinlge_line(called_numbers,user_bingo_book) or len(called_numbers) == 20:
                 self.set_state("matched_single_line")
         elif self.state == "matched_single_line":
+            # notify user they matched a single line and then pause calling numbers with input till user is ready
             print("Congratulations! You matched a line.")
             input("Press enter to continue: ")
             print("Now we are looking for two lines.")
             self.set_state("match_two_line")
         elif self.state == "match_two_line":
+            # check for 2 lines
             if check_for_two_lines(called_numbers,user_bingo_book) or len(called_numbers) == 40:
                 self.set_state("matched_two_lines")
-
         elif self.state == "matched_two_lines":
+            # notify user they have matched two lines
             print("Congratulations! You have matched two lines.")
             input("Press enter to continue: ")
             print("Now we are looking for a Full House.")
             self.set_state("match_full_house")
         elif self.state == "match_full_house":
+            # check for full house
             if check_for_fullhouse(called_numbers,user_bingo_book) or len(called_numbers) == 87:
                 self.set_state("matched_full_house")
         elif self.state == "matched_full_house":
+            # notify user has matched full house
             print("Congratulations! You have a full house.")
             input("Press enter to continue: ")
             self.set_state("end")
         elif self.state == "end":
+            # notify user game is over
             print("Game over.")
             input("Press enter to go to menu: ")
             self.set_state("menu")
         elif self.state == "break":
+            # lets the user take break after 50 numbers are called
             print("We will take a short brake now")
-
+            # pause the number calls loop 
             input("Press enter to continue")
         
