46e125f71c7d61faecf68785bee35903f9ad9e53
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-23 17:14:02 +0100

    Refactor: changed functions called for the 3 functions into one in check for win module
    Also added user bingo books to __init__ function so its created when bingogame object is created

--- game_state.py
+++ game_state.py
@@ -4,9 +4,13 @@ import number_generator
 from google_sheets import search_woksheet
 from create_book import convert_string_to_list
 from google_sheets import search_woksheet
+
+
+
 class BingoGame:
     def __init__(self):
         self.state = "menu"  # Initial state is "menu"
+        self.user_bingo_book = [] 
 
     def set_state(self, new_state):
         """sets the state the game is in"""
@@ -15,13 +19,13 @@ class BingoGame:
     def play(self):
         """changes the functionality of the game based on the state e.g. looking for single line, double line, full house,break or play"""
         called_numbers = number_generator.get_callednumbers_list()
-        user_bingo_book = []
         if self.state == "menu":
             display_menu.menu()
         elif self.state =="play":
             print("Please enter your book id to start the game")
             book_id = input("Book ID:\n")
-            user_bingo_book = convert_string_to_list(search_woksheet(book_id))   
+
+            self.user_bingo_book = convert_string_to_list(search_woksheet(book_id))  
             self.set_state("calling_numbers")
             self.play()
         elif self.state == "calling_numbers":
@@ -31,7 +35,8 @@ class BingoGame:
             number_generator.call_numbers()
         elif self.state == "match_single_line":
             # check for single line
-            if check_for_sinlge_line(called_numbers,user_bingo_book) or len(called_numbers) == 20:
+            match =  check_matching_row(called_numbers,self.user_bingo_book,1)
+            if match:
                 self.set_state("matched_single_line")
         elif self.state == "matched_single_line":
             # notify user they matched a single line and then pause calling numbers with input till user is ready
@@ -41,7 +46,7 @@ class BingoGame:
             self.set_state("match_two_line")
         elif self.state == "match_two_line":
             # check for 2 lines
-            if check_for_two_lines(called_numbers,user_bingo_book) or len(called_numbers) == 40:
+            if check_matching_row(called_numbers,self.user_bingo_book,2):
                 self.set_state("matched_two_lines")
         elif self.state == "matched_two_lines":
             # notify user they have matched two lines
@@ -51,7 +56,7 @@ class BingoGame:
             self.set_state("match_full_house")
         elif self.state == "match_full_house":
             # check for full house
-            if check_for_fullhouse(called_numbers,user_bingo_book) or len(called_numbers) == 87:
+            if check_matching_row(called_numbers,self.user_bingo_book,3):
                 self.set_state("matched_full_house")
         elif self.state == "matched_full_house":
             # notify user has matched full house
