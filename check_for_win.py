7ebd6b93a5ca381457575949c0287e1dcc76b9f8
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-08-23 17:03:51 +0100

    Refactor: merged 3 functions into one

--- check_for_win.py
+++ check_for_win.py
@@ -1,41 +1,23 @@
 
+def check_matching_row(numbers_called,bingo_book,amount_to_win):
+    """checks the called numbers against each row in the users bingo book and returns true if amount of rows to win in a box of 3 rows match"""
 
-def check_for_sinlge_line(numbers_called, bingo_book):
-    """checks the called numbers against each row in the users bingo book and returns true if any single row matches"""
-    if len(numbers_called) >= 5:
-        for row in bingo_book:
-            if all(number in numbers_called for number in row):
-                return True
-            else:
-                return False
-            
-def check_for_two_lines(numbers_called, bingo_book):
-     """checks the called numbers against each row in the users bingo book and returns true if two rows in a block of 3 match"""
-     winning_row_count = 0
-     winning_row_index = 0
-     if len(numbers_called) >= 10:
-        for index, row in enumerate(bingo_book):
-            if all(number in numbers_called for number in row):
-                winning_row_count += 1
-
-                if winning_row_count == 2 and index - winning_row_index < 3:
-                    return True
-                
-                winning_row_index = index
-
-def check_for_fullhouse(numbers_called, bingo_book):
-    """checks the called numbers against each row in the users bingo book and returns true if 3 rows in a row match"""
     winning_row_count = 0
-    full_house_counter =0
-    if len(numbers_called) >= 15:
-        for row in bingo_book:
-            if full_house_counter == 3:
-                full_house_counter =0
-            if all(number in numbers_called for number in row):
-                winning_row_count += 1
+    indices_to_skip = [0, 4, 8, 12,16,20]
+    if len(numbers_called) >= 5*amount_to_win:
 
-                if winning_row_count == 3 and full_house_counter == 2:
-                    print("win")
-            full_house_counter += 1
+        for index, row in enumerate(bingo_book):
+            if index in indices_to_skip or index == 0:   # skip row with id
+                winning_row_count = 0 # set winning row count back to 0 after every 3 rows lines need to be matched in a box of 3
+            else:
+                numbers_in_mixed_list = [item for item in row if isinstance(item, int)]
+                match = all(number in numbers_called for number in numbers_in_mixed_list)
                 
+                if match:
+                    print(numbers_in_mixed_list)
+                    winning_row_count += 1
+
+            if amount_to_win == winning_row_count:
+                return True
             
+
